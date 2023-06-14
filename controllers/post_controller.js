69069b3d77d1d6a9e83e7a69f746214313df649d
pranavvarshney18 <pranavvarshney18@gmail.com>
2023-06-14 17:11:19 +0530

    deleting post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 //create a new post
 module.exports.create = (req, res, next) => {
@@ -15,3 +16,33 @@ module.exports.create = (req, res, next) => {
         res.redirect('back');
     })
 };
+
+
+//delete post
+module.exports.destroy = (req, res, next) => {
+    Post.findById(req.params.id)
+        .exec()
+        .then(post => {
+            if(post && post.user == req.user.id){
+                post.deleteOne();
+
+                Comment.deleteMany({post: req.params.id})
+                    .then(deletedCount => {
+                        return res.redirect('back');
+                    })
+                    .catch(err => {
+                        console.log('error in deleting related comments for this post', err);
+                        return res.redirect('back');
+                    })
+                        
+            }
+            else {
+                console.log('unable to delete post');
+                return res.redirect('back');
+            }
+        })
+        .catch(err => {
+            console.log('error in finding the required post for deletion', err);
+            return res.redirect('back');
+        })
+}
--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,10 @@ const passport = require('passport');
 
 const postController = require('../controllers/post_controller');
 
+//to create post
 router.post('/create', passport.checkAuthentication, postController.create);
+
+//to delete post
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 
 module.exports = router;
