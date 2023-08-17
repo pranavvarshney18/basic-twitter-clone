7b114bbb9fdfd1c7b146180b7744d00c5463dad8
pranavvarshney18 <pranavvarshney18@gmail.com>
2023-08-17 08:47:29 +0530

    CHATTING ENGINE :: Time to create and join chat rooms

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -16,8 +16,21 @@ class ChatEngine{
 
 
     connectionHandler(){
+        let self = this;
+
         this.socket.on('connect', function(){
             console.log('connection established using sockets....!');
+
+            //request to join the room and send some data in the room
+            self.socket.emit('join_room', {
+                user_email: self.userEmail,
+                chatroom: 'codeial'
+            });
+
+            //after the server acknowledge the joining room request, we will come here
+            self.socket.on('user_joined', function(data){
+                console.log('a user joined', data);
+            })
         });
     }
 }
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -15,6 +15,17 @@
             console.log('socket disconnected!');
         });
        
+        //observer will receive request from user to join the room
+        socket.on('join_room', function(data){
+            console.log('joining request rec.', data);
+
+            //joining the room, socket will search for this chatroom,
+            //if it exist, then user will join,
+            //if it does not exist, then observer will create one and then the user will join
+            socket.join(data.chatroom);
+            //telling everyone in the room that xyz user has joined
+            io.in(data.chatroom).emit('user_joined', data);
+        })
     });
 
  }
